98a22f5f6ed9bfb87af37719bf35e5cbb72d9536
Bilol <sayidmurod2006@gmail.com>
2026-06-16 14:28:10 +0500

    remove site scroll

--- src/App.vue
+++ src/App.vue
@@ -8,3 +8,18 @@
 import BottomNavigation from '@/components/layout/BottomNavigation.vue'
 import TopNavigation from '@/components/layout/TopNavigation.vue'
 </script>
+
+<style>
+html, body {
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  height: 100%;
+  overflow: hidden; 
+}
+
+#app {
+  width: 100%;
+  height: 100%;
+}
+</style>
