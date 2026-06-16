6c81935e242c66e8959d1a89d9568e40626ae402
Bilol <sayidmurod2006@gmail.com>
2026-06-16 12:26:59 +0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<template>
+  <h1 class="hidden-sm-and-down">hello world</h1>
+  <BottomNavigation class="hidden-md-and-up"/>
+</template>
+
+<script lang='ts' setup>
+import BottomNavigation from '@/components/layout/BottomNavigation.vue'
+</script>
---
+++ src/components/layout/BottomNavigation.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-app>
+    <v-container>
+      <v-bottom-navigation :elevation="6" grow>
+        <v-btn value="home">
+          <v-icon>mdi-home</v-icon>
+
+          <span>Home</span>
+        </v-btn>
+
+        <v-btn value="map">
+          <v-icon>mdi-map</v-icon>
+
+          <span>Map</span>
+        </v-btn>
+
+        <v-btn value="bron">
+          <v-icon>mdi-lock</v-icon>
+
+          <span>Bron</span>
+        </v-btn>
+
+        <v-btn value="account">
+          <v-icon>mdi-account</v-icon>
+
+          <span>Account</span>
+        </v-btn>
+      </v-bottom-navigation>
+    </v-container>
+  </v-app>
+</template>
